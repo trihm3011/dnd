0f2b28ff8676a2603a08bc7ea99dd2cbf0147d43
trihm3011 <tri.hoang@hotschedules.com>
2017-07-06 15:20:34 +0700

    sortable simple es6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import SortableSimple from './components/SortableSimple';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<SortableSimple />, document.getElementById('root'));
 registerServiceWorker();
