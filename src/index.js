3a7c202f4990cff0a7f4d43b48dd25eb108293c2
trihm3011 <tri.hoang@hotschedules.com>
2017-07-07 12:04:26 +0700

    - simple target

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import SortableSimple from './04 Sortable/Simple';
+// import SortableSimple from './04 Sortable/Simple';
 // import HandlesAndPreviews from './05 Customize/Handles and Previews';
+import SimpleTarget from './01 Dustbin/Single Target';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<SortableSimple />, document.getElementById('root'));
+// ReactDOM.render(<SortableSimple />, document.getElementById('root'));
 // ReactDOM.render(<HandlesAndPreviews />, document.getElementById('root'));
+ReactDOM.render(<SimpleTarget />, document.getElementById('root'));
 registerServiceWorker();
